b9db23c76c9fc4d027b63bb421dae0685d6d7792
skprot <sayanprotasov@gmail.com>
2021-04-18 20:44:55 +0300

    docker sh script

--- rosrun.sh
+++ rosrun.sh
@@ -2,4 +2,4 @@
 
 echo "RUNNING ROS CAMERA NODE!"
 bash /ros_entrypoint.sh
-rosrun /root/catkin_ws/src/raspi_camera/launch/raspi_camera.launch
+roslaunch /root/catkin_ws/src/raspi_camera/launch/raspi_camera.launch
